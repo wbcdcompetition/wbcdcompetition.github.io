e86764f465040f653ddaadddb5a2705558f5da48
Lingfeng <lingfengsun@berkeley.edu>
2026-01-14 21:30:14 -0500

    Fix relative paths for thumbnails and RRD files

--- umi-gallery/src/data/datasets.js
+++ umi-gallery/src/data/datasets.js
@@ -8,8 +8,8 @@ export const DATASETS = [
     name: 'Clean Bowl',
     source: 'Genrobot',
     description: 'Bimanual bowl cleaning task demonstrating coordinated manipulation',
-    thumbnail: '/thumbnails/clean_bowl.jpg',
-    rrdUrl: '/rrd/clean_bowl.rrd',
+    thumbnail: './thumbnails/clean_bowl.jpg',
+    rrdUrl: './rrd/clean_bowl.rrd',
     topics: {
       transforms: ['/robot0/vio/eef_pose', '/robot1/vio/eef_pose'],
       cameras: ['/robot0/sensor/camera0', '/robot1/sensor/camera0'],
@@ -20,8 +20,8 @@ export const DATASETS = [
     name: 'Unscrew Bottle Cap and Pour',
     source: 'Genrobot',
     description: 'Fine manipulation task: unscrewing cap and pouring liquid',
-    thumbnail: '/thumbnails/unscrew_bottle_cap_and_pour.jpg',
-    rrdUrl: '/rrd/unscrew_bottle_cap_and_pour.rrd',
+    thumbnail: './thumbnails/unscrew_bottle_cap_and_pour.jpg',
+    rrdUrl: './rrd/unscrew_bottle_cap_and_pour.rrd',
     topics: {
       transforms: ['/robot0/vio/eef_pose', '/robot1/vio/eef_pose'],
       cameras: ['/robot0/sensor/camera0', '/robot1/sensor/camera0'],
@@ -32,8 +32,8 @@ export const DATASETS = [
     name: 'Zip Clothes',
     source: 'Genrobot',
     description: 'Precision zipping task with deformable garment handling',
-    thumbnail: '/thumbnails/zip_clothes.jpg',
-    rrdUrl: '/rrd/zip_clothes.rrd',
+    thumbnail: './thumbnails/zip_clothes.jpg',
+    rrdUrl: './rrd/zip_clothes.rrd',
     topics: {
       transforms: ['/robot0/vio/eef_pose', '/robot1/vio/eef_pose'],
       cameras: ['/robot0/sensor/camera0', '/robot1/sensor/camera0'],
